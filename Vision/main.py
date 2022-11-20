d98eb2715198e57dd342f02d9ea9bf00a72e9a87
Jaro167 <wyattjfshaw@gmail.com>
2022-11-20 13:16:11 -0400

    Update main.py

--- Vision/main.py
+++ Vision/main.py
@@ -1,8 +1,20 @@
 from RoboSub import challenge
+import rospy
+from std_msgs.msg import String
+
 
 Step1 = challenge()
+# Publisher Function
+def talker():
+    pub = rospy.Publisher('vision_py', String, queue_size = 25)
+    rospy.init_node('vision_py', anonymous=True)
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        #Vision Code
+        Step1.GetCentroidLocation(pub)  
+        
 
-Step1.ShowImageInColour("Marker.jpg")
 
-Step1.DisplayCentroidLocation()
+
+
 
